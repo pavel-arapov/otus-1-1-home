ef3eceab441baf126489c6a635955c18c35ad107
pavel <pavel.arapov@gmail.com>
2020-10-18 20:38:56 +0100

    chore(): home work 1

---
+++ Assets/Scripts/CharacterAnimationEvents.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimationEvents : MonoBehaviour
+{
+    Character character;
+
+    void Start()
+    {
+        character = GetComponentInParent<Character>();
+    }
+
+    void ShootEnd()
+    {
+        character.SetState(Character.State.Idle);
+        character.KillEnemy();
+    }
+
+    void AttackEnd()
+    {
+        character.SetState(Character.State.RunningFromEnemy);
+        character.KillEnemy();
+    }
+}
